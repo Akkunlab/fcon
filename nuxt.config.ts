06fd62114f83dea0892d4a6dd86e966270b155cc
Akkun <akkunlab@gmail.com>
2023-05-25 20:24:20 +0900

    Add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1.0', 
+      title: 'My App',
+    }
+  }
 })
