cb41f024bdf9e79b9842d2f768f139ea0fedde68
Akkun <akkunlab@gmail.com>
2023-06-04 21:35:27 +0900

    Add quiz post api

---
+++ server/api/quizzes.post.ts
@@ -0,0 +1,25 @@
+/* ============================================
+No.3
+Method: POST
+URI: /api/quizzes
+Description: 新しいクイズを作成
+============================================ */
+
+import { db } from '../lib/firestore';
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+
+export default defineEventHandler(async (event) => {
+
+  try {
+    const body = await readBody(event); // リクエストボディを取得
+
+    body.createdAt = Timestamp.fromDate(new Date()); // 作成日時を追加
+    body.updatedAt = Timestamp.fromDate(new Date()); // 更新日時を追加
+
+    const docRef = await addDoc(collection(db, 'quizzes'), body); // クイズのコレクションにドキュメントを追加
+
+    return { result: 'success' };
+  } catch (error) {
+    return error;
+  }
+})
