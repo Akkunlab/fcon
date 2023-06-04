f1f90fe5900281aa7bd5888be5457af455fe6af6
Akkun <akkunlab@gmail.com>
2023-06-04 19:32:10 +0900

    Add quizzes get api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    firebaseApiKey: process.env.FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.FIREBASE_APP_ID,
+  },
   css: ["vuetify/lib/styles/main.sass"],
   build: {
     transpile: ["vuetify"],
---
+++ server/api/quizzes.get.ts
@@ -0,0 +1,25 @@
+/* ============================================
+No.1
+Method: GET
+URI: /api/quizzes
+Description: すべてのクイズを取得
+============================================ */
+
+import { db } from '../lib/firestore';
+import { collection, getDocs, query } from 'firebase/firestore';
+
+export default defineEventHandler(async () => {
+
+  try {
+    const snapshot = await getDocs(query(collection(db, 'quizzes'))); // クイズのコレクションを取得
+    const docs = Array.from(snapshot.docs).map((doc) => {
+      return {
+        ...doc.data()
+      };
+    });
+
+    return docs;
+  } catch (error) {
+    return error;
+  }
+})
