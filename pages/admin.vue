c062b0b955b3ffcd61b6d3f3372bc9591d9ff6cd
Akkun <akkunlab@gmail.com>
2023-06-06 01:36:45 +0900

    Update dialog to create new quiz

--- pages/admin.vue
+++ pages/admin.vue
@@ -40,12 +40,54 @@
 
         <!-- 新規作成 -->
         <v-col cols="12" sm="6" md="4" lg="2">
-          <v-card class="new" height="130">
-            <v-card-title>
-              <v-icon style="margin-left: -8px;">mdi-plus</v-icon>
-              <span>New</span>
-            </v-card-title>
-          </v-card>
+          <v-dialog v-model="dialog" width="auto">
+            <template v-slot:activator="{ props }">
+              <v-card class="new" height="130" v-bind="props">
+                <v-card-title>
+                  <v-icon style="margin-left: -8px;">mdi-plus</v-icon>
+                  <span>New</span>
+                </v-card-title>
+              </v-card>
+            </template>
+            
+            <!-- 新規作成 : ダイアログ -->
+            <v-card class="pa-2" style="border-radius: 15px; background: #fff;">
+
+              <v-card-title class="text-h5">新規クイズ作成</v-card-title>
+
+              <v-card-text class="pa-0">
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field label="タイトル*" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field label="問題文*" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field label="問題の答え*" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-select :items="[1, 2, 3, 4, 5]" label="難易度*" required></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-autocomplete
+                        :items="['数学', '物理', '化学', '生物', '地学', '情報', '英語', '国語', '歴史', '地理', 'その他']"
+                        label="カテゴリ*" multiple>
+                      </v-autocomplete>
+                    </v-col>
+                  </v-row>
+                </v-container>
+                <small class="pa-3">*は必須フィールドです</small>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="dialog = false">Save</v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-dialog>
         </v-col>
       </v-row>
 
@@ -77,6 +119,9 @@
     { icon: 'mdi-help-circle', text: 'Quizzes' },
     { icon: 'mdi-account', text: 'Users' }
   ];
+
+  /* ダイアログ */
+  const dialog = useState('dialog', () => false);
 </script>
 
 <style lang="scss" scoped>
