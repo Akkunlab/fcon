6c31dbf8f20bec5396b136cbd7d5caa6732ebc91
Akkun <akkunlab@gmail.com>
2023-06-06 19:08:08 +0900

    Update app bar

---
+++ components/TheAppBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app-bar density="compact" color="rgba(255, 255, 255, 0.9)">
+    <v-app-bar-title class="font-weight-bold">Quizzes</v-app-bar-title>
+  </v-app-bar>
+</template>
--- pages/admin.vue
+++ pages/admin.vue
@@ -4,9 +4,7 @@
   <TheNav />
 
   <!-- トップバー -->
-  <v-app-bar density="compact" color="rgba(255, 255, 255, 0.9)">
-    <v-app-bar-title class="font-weight-bold">Quizzes</v-app-bar-title>
-  </v-app-bar>
+  <TheAppBar />
 
   <!-- メイン -->
   <v-main>
